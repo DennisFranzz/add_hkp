ad06d13c5732609341ad3c7dfc8934e0730fcc17
Dennis <yours91815@gmail.com>
2018-09-27 17:02:07 +0800

    db schema

--- hkproperty/schema.sql
+++ hkproperty/schema.sql
@@ -1,17 +1,108 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS agent;
+DROP TABLE IF EXISTS branch;
+DROP TABLE IF EXISTS property;
+DROP TABLE IF EXISTS propertyAddress;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS preference;
+DROP TABLE IF EXISTS transaction;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+
+CREATE TYPE transactionType AS ENUM ('sale','rent','both');
+CREATE TYPE title AS ENUM ('Mr.','Ms.','Mrs.');
+
+CREATE TABLE user(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR (50) UNIQUE NOT NULL,
+	password VARCHAR (50) NOT NULL,
+	email VARCHAR (355) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	is_superuser BOOLEAN DEFAULT FALSE,
+	last_login TIMESTAMP
 );
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
-);
+CREATE TABLE agent(
+	branch_id INTEGER NOT NULL,
+	managing_branch_id INTEGER,
+	CONSTRAINT agent_branch_id_fk FOREIGN KEY (branch_id)
+		REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT agent_managing_branch_id_fk FOREIGN KEY (manage_branch_id)
+		REFERENCES branch (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+	
+)INHERITS user
+
+CREATE TABLE branch(
+	id SERIAL PRIMARY KEY,
+	manager_id INTEGER NOT NULL,
+	CONSTRAINT branch_manager_id_fk FOREIGN KEY (manager_id)
+		REFERENCES agent (id) 
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+CREATE TABLE property(
+	id SERIAL PRIMARY KEY,
+	owner_id INTEGER NOT NULL,
+	address_id INTEGER NOT NULL,
+	gross_floor_area INTEGER NOT NULL,
+	number_of_bedrooms INTEGER NOT NULL,
+	provide_car_park BOOLEAN NOT NULL,
+	selling_price NUMERIC(20,2) NOT NULL,
+	rental_price NUMERIC(20,2) NOT NULL,
+	for_transaction_type transactionType NOT NULL,
+	CONSTRAINT property_address_id_fk FOREIGN KEY (address_id)
+		REFERENCES propertyAddress(id) 
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+CREATE TABLE propertyAddress(
+	id SERIAL PRIMARY KEY,
+	district VARCHAR(50) NOT NULL,
+	estate VARCHAR(50),
+	block VARCHAR(50) NOT NULL,
+	floor INTEGER NOT NULL,
+	flat VARCHAR(3) NOT NULL
+)
+
+CREATE TABLE customer(
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	title title,
+	phone VARCHAR(20) NOT NULL,
+	email VARCHAR(355),
+	preference_id INTEGER,
+	CONSTRAINT customer_preference_id_fk FOREIGN KEY (preference_id)
+		REFERENCES preference(id) 
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+CREATE TABLE preference(
+	id SERIAL PRIMARY KEY,
+	district VARCHAR(50),
+	estate VARCHAR(50),
+	buying_budget INTEGER,
+	rental_budget INTEGER,
+	transactionType transactionType
+)
+
+CREATE TABLE transaction(
+	ref SERIAL PRIMARY KEY,
+	type transactionType NOT NULL,
+	transaction_date TIMESTAMP NOT NULL,
+	property_id INTEGER NOT NULL,
+	sold_price NUMERIC(20,2),
+	rental_price NUMERIC(20,2),
+	owner_id INTEGER NOT NULL,
+	customer_id INTEGER NOT NULL,
+	agent_id INTEGER NOT NULL,
+	commission  NUMERIC(20,2) NOT NULL,
+	CONSTRAINT transaction_property_id FOREIGN KEY (property_id)
+		REFERENCES property(id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT transaction_owner_id FOREIGN KEY (owner_id)
+		REFERENCES customer(id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT transaction_customer_id FOREIGN KEY (customer_id)
+		REFERENCES customer(id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT transaction_agent_id FOREIGN KEY (agent_id)
+		REFERENCES agent(id) ON UPDATE NO ACTION ON DELETE NO ACTION	
+)
+
