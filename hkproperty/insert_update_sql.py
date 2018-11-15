5e640f416e2d9824496c851cc662ba6405d26b8b
Dennis <yours91815@gmail.com>
2018-11-15 14:42:23 +0800

    insert_update_sql

--- hkproperty/insert_update_sql.py
+++ hkproperty/insert_update_sql.py
@@ -1,61 +1,90 @@
-INSERT_USER = (
-	'INSERT INTO hkpUser(username,password,usergroup) values'
-	'(:username, :password, :admin);'
+UPSERT_USER = (
+	'INSERT INTO hkpUser(id, username,password,usergroup) values '
+	'(:id, :username, :password, :usergroup) '
+	'ON CONFLICT (id) DO UPDATE '
+  	'SET username = :username, password = :password, '
+	'usergroup = :usergroup;'
 )
 
-INSERT_BRANCH = (
-	'INSERT INTO branch(manager_id,address) values '
-	'(:manager_id, :address);'
+UPSERT_BRANCH = (
+	'INSERT INTO branch(id, manager_id,address) values '
+	'(:id, :manager_id, :address) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET manager_id = :manager_id, address = :address;'
 )
 
-INSERT_AGENT = (
-	'INSERT INTO agent(name, username,password,email,phone,usergroup,branch_id) values '
-	'(:name, :username, :password, :email, :phone, :usergroup, :branch_id);'
+UPSERT_AGENT = (
+	'INSERT INTO agent(:agent_id, name, username,password,email,phone,usergroup,branch_id) values '
+	'(:agent_id, :name, :username, :password, :email, :phone, :usergroup, :branch_id) '
+	'ON CONFLICT (agent_id) DO UPDATE '
+	'SET name = :name, password = :password, email = :email, phone = :phone, '
+	'usergroup = :usergroup, branch_id = :branch_id;'
 )
 
-INSERT_DISTRICT = (
-	'INSERT INTO district(name) values '
-	'(:name);'
+UPSERT_DISTRICT = (
+	'INSERT INTO district(id, name) values '
+	'(:id, :name) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET name= :name;'
 )
 
-INSERT_ESTATE = (
-	'INSERT INTO dstate(name) values '
-	'(:name);'
+UPSERT_ESTATE = (
+	'INSERT INTO estate(id, name) values '
+	'(:id, :name) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET name= :name;'
 )
 
-INSERT_PREFERENCE = (
-	'INSERT INTO preference(district_id, estate_id, buying_budget, rental_budget, transactionType) values '
-	'(:district_id, :estate_id, :buying_budget, :rental_budget, :trans_type);'
+UPSERT_PREFERENCE = (
+	'INSERT INTO preference(id, district_id, estate_id, buying_budget, rental_budget, transactionType) values '
+	'(:id, :district_id, :estate_id, :buying_budget, :rental_budget, :trans_type) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET district = :district_id, estate_id = :estate_id, buying_budget = :buying_budget, '
+	'rental_budget = :rental_budget, transactionType = :trans_type;'
 )
 
-INSERT_CUSTOMER = (
-	'INSERT INTO customer(name, title, phone, preference_id) values '
-	'(:name, :title, :phone, :preference_id);'
+UPSERT_CUSTOMER = (
+	'INSERT INTO customer(id, name, title, phone, preference_id) values '
+	'(:id, :name, :title, :phone, :preference_id) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET name = :name, title = :title, phone = :phone, preference_id = :preference_id;'
 )
 
-INSERT_PROPERTY_ADDRESS = (
-	'INSERT INTO propertyAddress(district_id, estate_id, block, floor, flat) values '
-	'(:district_id, :estate_id, :block, :floor, :flat);'
+UPSERT_PROPERTY_ADDRESS = (
+	'INSERT INTO propertyAddress(id, district_id, estate_id, block, floor, flat) values '
+	'(:id, :district_id, :estate_id, :block, :floor, :flat) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET district_id = :district_id, estate_id = :estate_id, block = :block, '
+	'floor = :floor, flat = :flat;'
 )
 
-INSERT_PROPERTY_OWNER = (
-	'INSERT INTO propertyOwner(name, title, phone) values '
-	'(:name, :title, :phone);'
+UPSERT_PROPERTY_OWNER = (
+	'INSERT INTO propertyOwner(id, name, title, phone) values '
+	'(:id, :name, :title, :phone) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET name = :name, title = :title, phone = :phone;'
 )
 
-INSERT_PROPERTY = (
-	'INSERT INTO property(owner_id, address_id, gross_floor_area, '
+UPSERT_PROPERTY = (
+	'INSERT INTO property(id, owner_id, address_id, gross_floor_area, '
 	'number_of_bedrooms, provide_car_park, selling_price, rental_price, '
 	'for_transaction_type) values '
-	'(:owner_id, :address_id, :gross_floor_area, :number_of_bedrooms,'
-	':provide_car_park, :selling_price, :rental_price, :trans_type'
-	');'
+	'(:id, :owner_id, :address_id, :gross_floor_area, :number_of_bedrooms,'
+	':provide_car_park, :selling_price, :rental_price, :trans_type) '
+	'ON CONFLICT (id) DO UPDATE '
+	'SET owner_id = :owner_id, address_id = :address_id, gross_floor_area = :gross_floor_area, '
+	'number_of_bedrooms = :number_of_bedrooms, provide_car_park = :provide_car_park, '
+	'selling_price = :selling_price, rental_price = :rental_price, for_transaction_type = :trans_type;'
 )
 
-INSERT_TRANSACTION = (
-	'INSERT INTO transaction( property_id, customer_id, agent_id, type, '
+UPSERT_TRANSACTION = (
+	'INSERT INTO transaction(ref_no, property_id, customer_id, agent_id, type, '
 	'transaction_date, rental_price, sold_price, commission) values '
-	'(:property_id, :customer_id, :agent_id, :type, :transaction_date, '
-	':rental_price, :sold_price, :commission'
-	');'
+	'(:ref_no, :property_id, :customer_id, :agent_id, :type, :transaction_date, '
+	':rental_price, :sold_price, :commission)'
+	'ON CONFLICT (ref_no) DO UPDATE '
+	'SET property_id = :property_id, customer_id = :customer_id, agent_id = :agent_id, '
+	'type = :type, transaction_date = :transaction_date, rental_price = :rental_price, '
+	'sold_price = :sold_price, commission = :commission'
+	';'
 )
